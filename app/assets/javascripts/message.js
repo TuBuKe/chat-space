45eaeb5a18708510364e13e9348209003044d735
TuBuKe <tubuke0657@gmail.com>
2019-08-23 17:51:07 +0900

    Create message.js
    JSON形式で帰ってきたデータを表示するHTMLの作成。

---
+++ app/assets/javascripts/message.js
@@ -0,0 +1,17 @@
+  function buildHTML(message){
+    var content = message.content ? `${ message.content }` : "";
+    var img = message.image ? `<img src= ${ message.image }>` : "";
+    var html = `<div class="message">
+                  <p class="message__user-name">
+                    ${message.name}
+                  </p>
+                  <p class="message__post-date">
+                    ${message.created_at}
+                  </p>
+                  <p class="message__latest-message">
+                    ${content}
+                    ${img}
+                  </p>
+                </div>`
+  return html;
+  }
